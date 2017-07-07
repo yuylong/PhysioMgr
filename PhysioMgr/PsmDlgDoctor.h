54622c19073a950f4f3da98dd4a220ba9ba495d9
Yulong Yu <yuylong@acm.org>
2017-07-07 16:39:23 +0800

    Add prototypes for doctor info dialog.

--- PhysioMgr/PsmDlgDoctor.h
+++ PhysioMgr/PsmDlgDoctor.h
@@ -2,6 +2,7 @@
 #define PSMDLGDOCTOR_H
 
 #include <QDialog>
+#include "PsmService.h"
 
 namespace Ui {
 class PsmDlgDoctor;
@@ -15,8 +16,33 @@ public:
     explicit PsmDlgDoctor(QWidget *parent = 0);
     ~PsmDlgDoctor();
 
+    PsmService *getService() const;
+    void setService(PsmService *service);
+
+    QString getDoctorId() const;
+    QString getDoctorName() const;
+    bool getIsNurse() const;
+    QString getType() const;
+    QString getDepartId() const;
+    QString getDepartName() const;
+    QString getPhoneNum() const;
+
+    void setDoctorId(const QString &id);
+    void setDoctorName(const QString &name);
+    void setIsNurse(bool isnurse);
+    void setType(const QString &type);
+    void setDepartId(const QString &depid);
+    void setDepartName(const QString &depname);
+    void setPhoneNum(const QString &phonenum);
+
+    void lockDoctorId();
+    void unlockDoctorId();
+
 private:
     Ui::PsmDlgDoctor *ui;
+    PsmService *service;
+
+    QString departId;
 };
 
 #endif // PSMDLGDOCTOR_H
