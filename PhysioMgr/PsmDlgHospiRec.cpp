b4d551f963e74b7783a43374c77433a8038cde02
Yulong Yu <yuylong@acm.org>
2017-07-12 16:35:02 +0800

    Implement some methods in hospital record dialog.

--- PhysioMgr/PsmDlgHospiRec.cpp
+++ PhysioMgr/PsmDlgHospiRec.cpp
@@ -15,3 +15,77 @@ PsmDlgHospiRec::~PsmDlgHospiRec()
 {
     delete ui;
 }
+
+QString PsmDlgHospiRec::getHospiRecId() const
+{
+    return ui->leHospiRecId->text();
+}
+
+QString PsmDlgHospiRec::getPatientId() const
+{
+    return ui->lePatientId->text();
+}
+
+QString PsmDlgHospiRec::getPatientName() const
+{
+    return ui->lePatientName->text();
+}
+
+QString PsmDlgHospiRec::getDepartId() const
+{
+    return this->depid;
+}
+
+QString PsmDlgHospiRec::getDepartName() const
+{
+    if (this->depid.isEmpty())
+        return QString();
+    else
+        return ui->pbDepart->text();
+}
+
+QString PsmDlgHospiRec::getRoomId() const
+{
+    return ui->leRoomId->text();
+}
+
+QString PsmDlgHospiRec::getDisease() const
+{
+    return ui->leDisease->text();
+}
+
+QString PsmDlgHospiRec::getDoctorId() const
+{
+    return this->doctorid;
+}
+
+QString PsmDlgHospiRec::getDoctorName() const
+{
+    if (this->doctorid.isEmpty())
+        return QString();
+    else
+        return ui->pbDoctor->text();
+}
+
+QString PsmDlgHospiRec::getNurseId() const
+{
+    return this->nurseid;
+}
+
+QString PsmDlgHospiRec::getNurseName() const
+{
+    if (this->nurseid.isEmpty())
+        return QString();
+    else
+        return ui->pbNurse->text();
+}
+
+QDate PsmDlgHospiRec::getStartDate() const
+{
+    return ui->deStartDate->date();
+}
+
+QDate PsmDlgHospiRec::getEndDate() const
+{
+    return ui->deEndDate->date();
+}
--- PhysioMgr/PsmDlgHospiRec.h
+++ PhysioMgr/PsmDlgHospiRec.h
@@ -2,6 +2,8 @@
 #define PSMDLGHOSPIREC_H
 
 #include <QDialog>
+#include <QString>
+#include "PsmService.h"
 
 namespace Ui {
 class PsmDlgHospiRec;
@@ -15,8 +17,27 @@ public:
     explicit PsmDlgHospiRec(QWidget *parent = 0);
     ~PsmDlgHospiRec();
 
+    QString getHospiRecId() const;
+    QString getPatientId() const;
+    QString getPatientName() const;
+    QString getDepartId() const;
+    QString getDepartName() const;
+    QString getRoomId() const;
+    QString getDisease() const;
+    QString getDoctorId() const;
+    QString getDoctorName() const;
+    QString getNurseId() const;
+    QString getNurseName() const;
+    QDate getStartDate() const;
+    QDate getEndDate() const;
+
 private:
     Ui::PsmDlgHospiRec *ui;
+    PsmService *service;
+
+    QString depid;
+    QString doctorid;
+    QString nurseid;
 };
 
 #endif // PSMDLGHOSPIREC_H
