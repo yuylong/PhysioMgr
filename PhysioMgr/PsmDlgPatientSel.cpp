687a876be972f6533d6fd2b7228d56bfe90ec845
Yulong Yu <yuylong@acm.org>
2017-07-19 16:39:33 +0800

    Implement the methods in patient selection dialog.

--- PhysioMgr/PsmDlgPatientSel.cpp
+++ PhysioMgr/PsmDlgPatientSel.cpp
@@ -6,9 +6,51 @@ PsmDlgPatientSel::PsmDlgPatientSel(QWidget *parent) :
     ui(new Ui::PsmDlgPatientSel)
 {
     ui->setupUi(this);
+    this->service = NULL;
 }
 
 PsmDlgPatientSel::~PsmDlgPatientSel()
 {
     delete ui;
+}
+
+PsmService *PsmDlgPatientSel::getService() const
+{
+    return this->service;
+}
+
+void PsmDlgPatientSel::setService(PsmService *service)
+{
+    this->service = service;
+}
+
+void PsmDlgPatientSel::setAndRefreshCond(const QString &cond)
+{
+    ui->leCond->setText(cond);
+    this->on_pbRefresh_clicked();
+}
+
+bool PsmDlgPatientSel::getSelectedPatient(PsmSrvPatient *patient)
+{
+    if (this->service == NULL || patient == NULL)
+        return false;
+
+    return this->service->readSelectedPatient(ui->tableWidget, patient);
+}
+
+void PsmDlgPatientSel::on_pbRefresh_clicked()
+{
+    if (this->service == NULL)
+        return;
+
+    if (ui->leCond->text().isEmpty())
+        this->service->refreshPatientList(ui->lblCount, ui->tableWidget, this);
+    else
+        this->service->searchPatient(ui->leCond->text(), ui->lblCount, ui->tableWidget, this);
+}
+
+void PsmDlgPatientSel::on_tableWidget_doubleClicked(const QModelIndex &index)
+{
+    ui->tableWidget->selectRow(index.row());
+    this->accept();
 }
--- PhysioMgr/PsmDlgPatientSel.h
+++ PhysioMgr/PsmDlgPatientSel.h
@@ -2,6 +2,7 @@
 #define PSMDLGPATIENTSEL_H
 
 #include <QDialog>
+#include "PsmService.h"
 
 namespace Ui {
 class PsmDlgPatientSel;
@@ -15,8 +16,20 @@ public:
     explicit PsmDlgPatientSel(QWidget *parent = 0);
     ~PsmDlgPatientSel();
 
+    PsmService *getService() const;
+    void setService(PsmService *service);
+
+    void setAndRefreshCond(const QString &cond);
+    bool getSelectedPatient(PsmSrvPatient *patient);
+
+private slots:
+     void on_pbRefresh_clicked();
+
+     void on_tableWidget_doubleClicked(const QModelIndex &index);
+
 private:
     Ui::PsmDlgPatientSel *ui;
+    PsmService *service;
 };
 
 #endif // PSMDLGPATIENTSEL_H
